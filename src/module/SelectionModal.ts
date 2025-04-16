98b828b091d547544fe6c20625a9169f61525832
Valerio <val.ferrandino@gmail.com>
2025-04-16 09:01:32 +0200

    SelectionModal rework

--- src/module/SelectionModal.ts
+++ src/module/SelectionModal.ts
@@ -5,25 +5,23 @@ import { ControllerUiState } from "./ControllerUiState";
 class SelectionModal extends SuggestModal<{ text: string, value: ControllerUiState }> {
     private modalData: { text: string, value: any }[] = [];
 
-    constructor(title: string, fields: { text: string, value: any }[]) {
+    constructor(title: string, fields: { text: string, value: ControllerUiState }[]) {
         super(FinancialTracker.PLUGIN_APP);
         this.setPlaceholder(title);
         this.modalData = fields;
     }
 
-    getSuggestions(query: string): { text: string; value: any; }[] | Promise<{ text: string; value: any; }[]> 
-    {
+    getSuggestions(query: string): { text: string; value: ControllerUiState; }[] | Promise<{ text: string; value: ControllerUiState; }[]> {
         // Filtering disabled
         return this.modalData;
     }
 
-    renderSuggestion(value: { text: string; value: any; }, el: HTMLElement): void 
-    {
+    renderSuggestion(value: { text: string; value: ControllerUiState; }, el: HTMLElement): void {
         el.createEl('div', { text: value.text });
     }
 
-    onChooseSuggestion(item: { text: string; value: any; }, evt: MouseEvent | KeyboardEvent): void {}
-    
+    onChooseSuggestion(item: { text: string; value: ControllerUiState; }, evt: MouseEvent | KeyboardEvent): void { }
+
 }
 
 
@@ -31,22 +29,29 @@ class SelectionModal extends SuggestModal<{ text: string, value: ControllerUiSta
  * Creates a modal with a list of entries and returns the selected value.
  * @param title The title of the modal.
  * @param entries The entries to display in the modal.
- * @returns A promise that resolves to the selected value.
+ * @param onSelect The function to call when an entry is selected.
+ * @param onClose The function to call when the modal is closed.
  */
-export async function createSelectionModal(title: string, entries: {text: string, value: ControllerUiState}[]): Promise<any> {
-    return new Promise(async (resolve) => {
-        const modal = new SelectionModal(title, entries);
-        let selected = false;
-        modal.onChooseSuggestion = (item) => {
-            selected = true;
-            resolve(item.value);
-        };
-        modal.onClose = () => {
-            setTimeout(() => {
-                if (selected) return;
-                resolve(`exit`);
-            }, 25);
-        }
-        modal.open();
-    });
+export function createSelectionModal(
+    title: string,
+    entries: { text: string, value: ControllerUiState }[],
+    onSelect: (item: any) => {},
+    onClose: () => {}
+): void {
+
+    const modal = new SelectionModal(title, entries);
+
+    let modalChooseFunction = (item: { text: string, value: ControllerUiState }) => {
+        modalCloseFunction = () => { };
+        onSelect(item.value);
+    }
+
+    let modalCloseFunction = () => {
+        onClose();
+    }
+
+    modal.onChooseSuggestion = modalChooseFunction;
+    modal.onClose = modalCloseFunction;
+    modal.open();
+
 }
